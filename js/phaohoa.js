c3a0d6c51df52fadd07ac8082a1393bf64bd512b
duytan12a7 <101546603+duytan12a7@users.noreply.github.com>
2023-02-06 16:37:19 +0700

    sss

--- js/phaohoa.js
+++ js/phaohoa.js
@@ -25,7 +25,7 @@ var canvas = document.getElementById('phaohoa'),
     limiterTotal = 5,
     limiterTick = 0,
     // this will time the auto launches of fireworks, one launch per 80 loop ticks
-    timerTotal = 10,
+    timerTotal = 5,
     timerTick = 0,
     mousedown = false,
     // mouse x coordinate,
